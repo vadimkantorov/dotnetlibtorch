1cacf38fb4d94aa0b977f0e9c4a8c1a9afdf91a4
Vadim Kantorov <vadimkantorov@gmail.com>
2020-08-17 23:00:05 +0000

    ...

--- dotnetlibtorch.cpp
+++ dotnetlibtorch.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 
 #include <torch/torch.h>
+#include <ATen/dlpack.h>
 #include <ATen/DLConvertor.h>
 
-extern "C"
+extern "C" DLManagedTensor process_dlpack_with_libtorch(DLManagedTensor dl_managed_tensor_in)
 {
+	torch::Tensor tensor = at::fromDLPack(&dl_managed_tensor_in);
 
-	#include "dlpack.h"
+	auto res = tensor + 1;
 
-	DLManagedTensor process_dlpack_with_libtorch(DLManagedTensor dl_managed_tensor_in)
-	{
-		torch::Tensor tensor = at::fromDLPack(&dl_managed_tensor_in);
-
-		auto res = tensor + 1;
-
-		return *at::toDLPack(res);
-	}
-
+	return *at::toDLPack(res);
 }
