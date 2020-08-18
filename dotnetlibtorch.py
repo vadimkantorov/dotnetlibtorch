ee81746c9421a95e1e8e51b5d20a793b75088242
Vadim Kantorov <vadimkantorov@gmail.com>
2020-08-18 17:23:49 +0200

    Update dotnetlibtorch.py

--- dotnetlibtorch.py
+++ dotnetlibtorch.py
@@ -2,9 +2,7 @@ import torch
 
 model = torch.nn.Linear(3, 6)
 
-example_input = torch.rand(16, model.weight.shape[1])
-
-traced_model = torch.jit.trace_module(model, example_input)
+traced_model = torch.jit.trace_module(model, example_inputs = torch.rand(16, model.weight.shape[1]))
 torch.jit.save(traced_model, 'jit_traced_model.pt')
 
 scripted_model = torch.jit.script(model)
